96c892ee6fec7e34bae45f9085f2565b32c53c6c
divya1995deepu <divyapuneeth21@gmail.com>
2025-01-06 17:06:09 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,25 +1,9 @@
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "allow ssh and http traffic"
+output "public_ip" {
+ value       = aws_instance.my_vm.public_ip
+ description = "Public IP Address of EC2 instance"
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+output "instance_id" {
+ value       = aws_instance.my_vm.id
+ description = "Instance ID"
 }
