a2b8694299d5ceb142a34e53422700e409d04737
divya1995deepu <divyapuneeth21@gmail.com>
2025-01-06 17:36:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-  }
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.18.0"
+   }
+ }
+
+ backend "s3" {
+   bucket = "my-tf-bucket-jenkins"
+   key    = "state"
+   region = "us-east-1"
+ }
+}
